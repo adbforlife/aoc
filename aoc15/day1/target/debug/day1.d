03fc0dee5ed3a7547c7401eeef71efe1ae8e3c3b
ADB <adbforlife2018@gmail.com>
2021-01-10 11:09:15 -0500

    Get rid of debug info

--- aoc15/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/adb/aoc/aoc15/day1/target/debug/day1: /home/adb/aoc/aoc15/day1/src/main.rs
--- aoc15/day1/target/debug/deps/day1-69904dfcf6a364ad.d
+++
@@ -1,5 +0,0 @@
-/home/adb/aoc/aoc15/day1/target/debug/deps/day1-69904dfcf6a364ad: src/main.rs
-
-/home/adb/aoc/aoc15/day1/target/debug/deps/day1-69904dfcf6a364ad.d: src/main.rs
-
-src/main.rs:
